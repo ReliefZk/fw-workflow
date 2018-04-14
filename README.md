8edca3846d6f270a4dc94e62535403681e40b2fc
ReliefZk <relifzk@foxmail.com>
2018-04-14 18:08:56 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# fw-workflow
+# fw-workflow (flyweight-workflow)
+蝇量级工作流引擎，不准守bpmn2.0规范
+
+配置：
+```
+    <fw-workflow:workflow id="testWorkflow">
+            <fw-workflow:gateway from="start" to="check" conditionExpr="${1=1}" postListener="listener1"/>
+            <fw-workflow:gateway from="check" to="confirm" springExprBean="checkExpr" preListener="listener2"/>
+    </fw-workflow:workflow>
+    
+    <bean id="checkExpr" class="com.reliefzk.middleware.workflow.WorkflowExprBean1"/>
+    <bean id="listener1" class="com.reliefzk.middleware.workflow.WorkflowListener1"/>
+    <bean id="listener2" class="com.reliefzk.middleware.workflow.WorkflowListener1"/>
+```
+调用：
+```
+WorkflowContext context = new WorkflowContext();
+context.put("", "");
+WorkflowResult result = fwWorkflowEngine.start("testWorkflow", "start", context);
+if(result.getSuccess()){
+    String nextStep = result.getTo();
+}
+```
--- src/test/java/com/reliefzk/middleware/workflow/WorkflowSchemaTest.java
+++ src/test/java/com/reliefzk/middleware/workflow/WorkflowSchemaTest.java
@@ -4,20 +4,37 @@ import java.util.Map;
 
 import com.alibaba.fastjson.JSON;
 
+import com.reliefzk.middleware.workflow.core.WorkflowContext;
 import com.reliefzk.middleware.workflow.core.definations.FwWorkflow;
+import com.reliefzk.middleware.workflow.engine.FwWorkflowEngine;
+import com.reliefzk.middleware.workflow.engine.WorkflowResult;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
  */
 public class WorkflowSchemaTest {
 
+    private FwWorkflowEngine fwWorkflowEngine;
+
+    @Test
+    public void testEngine(){
+        WorkflowContext context = new WorkflowContext();
+        context.put("", "");
+        WorkflowResult result = fwWorkflowEngine.start("testWorkflow", "start", context);
+        if(result.getSuccess()){
+            System.err.println(result.getTo());
+        }
+    }
+
 
     @Test
     public void testApp() {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("/spring/workflow/spring-context*.xml");
         Map<String, FwWorkflow> map = ctx.getBeansOfType(FwWorkflow.class);
+
         System.err.println(JSON.toJSONString(map.values()));
     }
 }
